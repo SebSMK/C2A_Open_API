aa1dc42676d78d0ce36decc22449a10d6389ede9
Seb <seb@smk.dk>
2016-11-04 11:38:43 +0100

    [FEATURE] psot_ocr return Kunstværk

--- api/controllers/post_proto_ocr.js
+++ api/controllers/post_proto_ocr.js
@@ -14,7 +14,7 @@ function post2OCR(req, res) {
   
   var params = req.body;
 
-  var imgB64 = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAAAAACPAi4CAAACOklEQVRYw+3VT0gUYRzG8ccNNliIQhT00NJhD+7BwOjPLeZaGFEIEaa4hRQd4gWhU2RghyAQCaKlqxhECy0RgoomBhFFRJdAoj9CYSnEmEpLbX47vDM77+xeO3SY5/a+O++H953f752VkiRJkiTJP8/eW282+bV0v7tJUpcJ0i4pHQ7ydrpL2m0nstHydJEwM62SCQdnJHWFgwE7baSbAKxkImCCKM9TEXBDUqEBSK8AcCVa7+HmdAQ8ljTWAPQAsNEcAXcAuNTZV7GrasAnSfMNwDQAY84bnAJYknQPYNUCbwF2SD7w3gWyWwBV5xVqDmBtuzTs+77/3QKP1oFD2gN8mXeBEQDG3RqOA/DsSDoYG4DyAjCo40DJBYY+A9DpAheCU/540L8zAkaBMQ0Dl11gFoDJWBdlPtRKsHk77INyLzCnh8BhF9gCwIv3Ye5VVMVvBwKgA1jRR6hmXMCmu66Tt52a/RP+9nWXBVLrQA54LRdYtO3WeBta+ksVKwxZQAvANaAYA65uNJ7B8zzP2y+1lAAoB8Ao8A4YiAHmrr0zLvAzbKR2AJ4GQG9wpnwc2GdnDzrAIkC1TWoFYCoAOuyTflMc0Au7Twew9+Vl34knAFwPgNQ6ANOqAwrhxmrJ/3ZvYyUbAFoAYKQeyPgATDhbcCvMOYWA3dnReiCYr+Yc4exauHz5pGpAHwAtDUDePlp0C9E8NLMKy5ODmfCbeExqM8aY85J64t9EFYwxxlxM/kqSJEmS5H/LXw/tL7IM9UBNAAAAAElFTkSuQmCC';
+  //var imgB64 = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAAAAACPAi4CAAACOklEQVRYw+3VT0gUYRzG8ccNNliIQhT00NJhD+7BwOjPLeZaGFEIEaa4hRQd4gWhU2RghyAQCaKlqxhECy0RgoomBhFFRJdAoj9CYSnEmEpLbX47vDM77+xeO3SY5/a+O++H953f752VkiRJkiTJP8/eW282+bV0v7tJUpcJ0i4pHQ7ydrpL2m0nstHydJEwM62SCQdnJHWFgwE7baSbAKxkImCCKM9TEXBDUqEBSK8AcCVa7+HmdAQ8ljTWAPQAsNEcAXcAuNTZV7GrasAnSfMNwDQAY84bnAJYknQPYNUCbwF2SD7w3gWyWwBV5xVqDmBtuzTs+77/3QKP1oFD2gN8mXeBEQDG3RqOA/DsSDoYG4DyAjCo40DJBYY+A9DpAheCU/540L8zAkaBMQ0Dl11gFoDJWBdlPtRKsHk77INyLzCnh8BhF9gCwIv3Ye5VVMVvBwKgA1jRR6hmXMCmu66Tt52a/RP+9nWXBVLrQA54LRdYtO3WeBta+ksVKwxZQAvANaAYA65uNJ7B8zzP2y+1lAAoB8Ao8A4YiAHmrr0zLvAzbKR2AJ4GQG9wpnwc2GdnDzrAIkC1TWoFYCoAOuyTflMc0Au7Twew9+Vl34knAFwPgNQ6ANOqAwrhxmrJ/3ZvYyUbAFoAYKQeyPgATDhbcCvMOYWA3dnReiCYr+Yc4exauHz5pGpAHwAtDUDePlp0C9E8NLMKy5ODmfCbeExqM8aY85J64t9EFYwxxlxM/kqSJEmS5H/LXw/tL7IM9UBNAAAAAElFTkSuQmCC';
   var blob = makeblob(req.body);
   
   var options = {
@@ -33,8 +33,27 @@ function post2OCR(req, res) {
   rp(options)  
   .then(function (parsedBody) {
       console.log(parsedBody);
-      res.json(JSON.parse(parsedBody));  
+      var qsolr = extractMSOCRResult(JSON.parse(parsedBody));
+      //res.json(JSON.parse('{"res":"' + qsolr.join() + '"}'));      
+      
+      
+      var smkOptions = {
+        method: 'POST',
+        uri: 'http://172.20.1.203:10011/api/solr',
+        
+        headers: {            
+            'Content-Type': 'application/json',                    
+        },
+        'body': '{"q":"' + qsolr.join(" ") + '","start":0,"rows":1,"fl":"id, title_first","defType":"edismax","qf":"collectorExact1^150 collectorExact2^30 collectorExact3^20 collector1^20 collector2^15 collector3^10 collector4^5"}'       
+      };
+      
+      return rp(smkOptions);
+         
   })
+  .then(function (parsedBody) {
+      console.log(parsedBody);
+      res.json(JSON.parse(parsedBody));     
+   })  
   .catch(function (error) {        
     console.log(error);
     res.status(error.status).json(error);        
@@ -44,6 +63,32 @@ function post2OCR(req, res) {
   /***
  *  PRIVATE FUNCTIONS
  **/
+
+function extractMSOCRResult(ocroutput){
+  var result = [];
+  if(ocroutput.hasOwnProperty('regions')){
+    var regions = ocroutput.regions; 
+    regions.forEach(function(item, index){        
+      if(regions[index].hasOwnProperty('lines')) {
+        var lines = regions[index].lines;
+        lines.forEach(function(item, index){
+          if(lines[index].hasOwnProperty('words')) {
+            var words = lines[index].words;
+            words.forEach(function(item, index){
+              var word = words[index];
+              if(word.hasOwnProperty('text'))
+                result.push(word.text);
+            })                       
+          }  
+        })         
+      }          
+    })
+  }
+    
+  return result;      
+}
+
+
 
 function makeblob (dataURL) {
     var BASE64_MARKER = ';base64,';
