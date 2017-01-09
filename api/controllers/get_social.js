a808737d9be03e0ad6aa3a5e6e3c5858d1d80a4a
Seb <seb@smk.dk>
2017-01-09 12:54:56 +0100

    [DRIFT] to branch

--- api/controllers/get_social.js
+++ api/controllers/get_social.js
@@ -33,11 +33,11 @@ module.exports = {
 
 function getsocial(req, res) {
 
- var iiiflink = req.swagger.params.refnum.value;
+ var iiiflink = decodeURI(req.swagger.params.refnum.value);
  var url = req.url;
  var host = req.headers.host;
  var image = 'http%3A%2F%2Fiip.smk.dk%2Fiiif%2F%252Fmnt%252Fdamapistorage%252Fdev%252Fkms1_564af3627fab5_pyr.tif%2F3112%2C1788%2C1838%2C1544%2F400%2C%2F0%2Fdefault.jpg';  
- image = encodeURI(iiiflink);
+ image = (iiiflink);
  var socialhtmlTemplate = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">  <html>    <head>       <title>%1$s</title>              <meta content="utf-8">       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">       <meta name="google" content="notranslate">             <meta property="og:image" content="%1$s">       <meta property="og:url" content="http://%3$s%4$s">                    <meta property="og:title" content="%1$s">                   <meta property="og:site_name" content="SMK">       <link rel="shortcut icon" href="http://demoapi.smk.dk/images/smk-favicon.png">       <link rel="apple-touch-icon" href="http://demoapi.smk.dk/images/smk.png">       <style type="text/css">body{          width:100%%;          height:100%%;          margin:0;          padding:0;           font-family: Verdana, Geneva, sans-serif;          }       </style>    </head>    <body>       <div id="map">%1$s</div>       <div id="viewer"><img src="%2$s"/></div>    </body> </html> ';
   
  var socialhtml = sprintf(socialhtmlTemplate, iiiflink, image, host, url);
